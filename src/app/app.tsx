aa5dddf1754657f39c736fc10fdee7eabe54bd2c
medanmilos1831 <medanmilos1831@gmail.com>
2024-12-06 13:09:15 +0100

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.less';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-event-mediator-provider" />
+    </div>
+  );
+}
+
+export default App;
