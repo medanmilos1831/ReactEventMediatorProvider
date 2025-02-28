4bfbc833363dc34ea7041c3fa0be68f950a1b617
medanmilos1831 <medanmilos1831@gmail.com>
2025-02-28 10:27:57 +0100

    polish scopes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -65,7 +65,7 @@ const HomePage = () => {
           dispatch({
             eventName: 'person',
             payload: {
-              fname: 'GLOBAL',
+              fname: 'GLOBAL scope',
             },
           });
         }}
