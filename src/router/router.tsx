44d18bf5a350e303e734b398b0f64e141f5aebe0
medanmilos1831 <medanmilos1831@gmail.com>
2025-04-21 15:53:00 +0200

    doc init

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,25 +1,77 @@
 import { createBrowserRouter, NavLink, Outlet } from 'react-router-dom';
+import { ScrollProvider } from '../components';
+import { Col, Row } from 'antd';
+import { Introduction } from '../pages/Introduction';
 
 export const router = () =>
   createBrowserRouter(
     [
       {
         element: (
-          <>
-            <div>
-              <NavLink to={'/'}>home</NavLink>
-              <NavLink to={'/subscribe'}>sub</NavLink>
-              <NavLink to={'/dispatch'}>dis</NavLink>
-            </div>
-            <div>
-              <Outlet />
-            </div>
-          </>
+          <div
+            style={{
+              height: '100vh',
+              width: '100vw',
+              background: '#0f0b2b',
+            }}
+          >
+            <Row
+              style={{
+                height: '100%',
+              }}
+            >
+              <Col
+                span={4}
+                style={{
+                  color: 'white',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: '100%',
+                  background: '#141226',
+                }}
+              >
+                <NavLink
+                  style={{
+                    color: 'white',
+                  }}
+                  to={'/'}
+                >
+                  Introduction
+                </NavLink>
+                <NavLink
+                  style={{
+                    color: 'white',
+                  }}
+                  to={'/subscribe'}
+                >
+                  sub
+                </NavLink>
+                <NavLink
+                  style={{
+                    color: 'white',
+                  }}
+                  to={'/dispatch'}
+                >
+                  dis
+                </NavLink>
+              </Col>
+              <Col
+                span={20}
+                style={{
+                  padding: '4rem',
+                }}
+              >
+                <ScrollProvider>
+                  <Outlet />
+                </ScrollProvider>
+              </Col>
+            </Row>
+          </div>
         ),
         children: [
           {
             index: true,
-            element: <>Home</>,
+            element: <Introduction />,
           },
           {
             path: '/subscribe',
